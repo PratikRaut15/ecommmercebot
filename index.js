9147362f073bf1a017d9bd94fd337873a0a9ee93
Pratik Raut <Pratik.Raut@lntinfotech.com>
2021-03-08 18:45:49 +0530

    adding the project code

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const dialogflow = require("./router/dialogflow");
+const azure = require("./router/azure");
+const bodyParser = require("body-parser");
+var path=require('path');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use("",dialogflow);
+app.use("",azure);
+
+
+
+app.listen(3000,() => console.log("running on 3000"));
+
+
+
+

e53aab539b0f49b21866aae7ab7e40f15dd3a520
Pratik Raut <Pratik.Raut@lntinfotech.com>
2021-03-11 10:21:18 +0530

    added endpoints for react

--- index.js
+++ index.js
@@ -1,18 +1,14 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const cors = require("cors");
+app.use(cors());
 const dialogflow = require("./router/dialogflow");
 const azure = require("./router/azure");
 const bodyParser = require("body-parser");
-var path=require('path');
+var path = require("path");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use("",dialogflow);
-app.use("",azure);
+app.use("", dialogflow);
+app.use("", azure);
 
-
-
-app.listen(3000,() => console.log("running on 3000"));
-
-
-
-
+app.listen(3000, () => console.log("running on 3000"));
